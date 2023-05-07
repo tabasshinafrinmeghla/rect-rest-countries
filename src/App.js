a3c92dfec68d995d3bc04bb3219de0c6affa1980
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2023-05-07 13:04:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadeCountries></LoadeCountries>
     </div>
   );
+}
+
+function LoadeCountries(){
+  // declaring state to save loaded data
+  const [countries,setCountries] = useState ([]);
+  // useEffect
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    // .then(data=>console.log(data))
+    .then(data=>setCountries(data))
+  }
+   ,[])
+  return(
+    <div>
+      <h1>hello every one</h1>
+      <h3>all countries: {countries.length} </h3>
+      {
+        countries.map (countrie=> <LoadeCountries name={countrie.name} ></LoadeCountries>)
+      }
+    </div> 
+  )
 }
 
 export default App;
